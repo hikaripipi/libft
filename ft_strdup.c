1f4bed3624ab3ca346e9ae5a8ed907ec6552e4cd
Hikari Shinagawa <hshinaga@c2r6s8.42tokyo.jp>
2024-11-02 15:58:56 +0900

    fixed missspell

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,27 +6,21 @@
 /*   By: hshinaga <hshinaga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 15:32:09 by hshinaga          #+#    #+#             */
-/*   Updated: 2024/11/02 15:35:30 by hshinaga         ###   ########.fr       */
+/*   Updated: 2024/11/02 15:53:27 by hshinaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
+#include "libft.h"
 
-int	main(void)
+char	*ft_strdup(const char *s1)
 {
-	const char	*test = "Hello, World!";
-	char		*dup;
-	const char	*empty = "";
-	char		*empty_dup;
+	char	*dup;
+	size_t	len;
 
-	dup = ft_strdup(test);
-	printf("Original: %s\n", test);
-	printf("Duplicate: %s\n", dup);
-	free(dup);
-	empty_dup = ft_strdup(empty);
-	printf("Empty Original: '%s'\n", empty);
-	printf("Empty Duplicate: '%s'\n", empty_dup);
-	free(empty_dup);
-	return (0);
+	len = ft_strlen(s1);
+	dup = (char *)malloc(sizeof(char) * (len + 1));
+	if (dup == NULL)
+		return (NULL);
+	ft_strlcpy(dup, s1, len + 1);
+	return (dup);
 }
